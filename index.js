21a55529c2285bca0a9bab33635e81b8bfaa76f5
Taylor <taybelle2020@gmail.com>
2024-10-08 11:15:01 -0400

    question 1 - 4 done

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 //Write your solutions in this file, don't forget to test your functions.
+
+const countFromOne = (num) => {
+  for (let i = 1; i <= num; i++) {
+    console.log(i);
+  }
+};
+console.log(countFromOne(10));
+
+// Question 2
+
+const countEveryOdd = (n) => {
+  for (let i = 0; i < 10; i++) {
+    if (i % 2 !== 0) {
+      console.log(i);
+    }
+  }
+};
+countEveryOdd(10);
+
+// Question 3
+
+const isNegative = (num) => {
+  if (num < 0) {
+    console.log(true);
+  } else {
+    return false;
+  }
+};
+isNegative(-8);
+console.log(isNegative(-8));
+
+// Question 4
+
+const betweenFiveAndTwenty = (num) => {
+  if (num >= 5 && num <= 20) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(betweenFiveAndTwenty(12));
